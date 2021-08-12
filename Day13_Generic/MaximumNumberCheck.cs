f3baec48a8b1fa176d7e64be8fc231aa784694c6
Your Name <you@example.com>
2021-08-12 11:14:22 +0530

    feat: find maximum number between three given number

---
+++ Day13_Generic/MaximumNumberCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Generic
+{
+     public  class MaximumNumberCheck
+    {
+       public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstNumber,SecondNumber,ThirdNumber are same ");
+
+
+
+        }
+
+
+      }
+}
--- Day13_Generic/Program.cs
+++ Day13_Generic/Program.cs
@@ -8,6 +8,12 @@ namespace Day13_Generic
         static void Main(string[] args)
         {
             Console.WriteLine("-----------------Welcoe to  find Maximum Number By using Generic------------------");
+             Console.WriteLine("Find Max Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(121, 28, 823);
+            Console.WriteLine(output);
+           
+
+
         }
     }
 }
