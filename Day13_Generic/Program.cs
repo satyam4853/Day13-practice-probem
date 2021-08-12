1a102f6ea756d8729a67be05a4898990bfbb8a7e
Your Name <you@example.com>
2021-08-12 11:01:36 +0530

    Greeting Message

---
+++ Day13_Generic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day13_Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------Welcoe to  find Maximum Number By using Generic------------------");
+        }
+    }
+}
