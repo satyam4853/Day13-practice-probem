196db916900dfaa41e577f7bba6974f124127673
Your Name <you@example.com>
2021-08-14 16:01:19 +0530

    Refactor : All three method refactor by using Generic one method

--- Day13_Generic/MaximumNumberCheck.cs
+++ Day13_Generic/MaximumNumberCheck.cs
@@ -1,74 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Day13_Generic
-{
-    public class MaximumNumberCheck
-    {
-        public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
-        {
-            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
-            {
-                return firstvalue;
+//namespace Day13_Generic
+//{
+//    public class MaximumNumberCheck
+//    {
+//        public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+//        {
+//            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+//            {
+//                return firstvalue;
 
-            }
-            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
-            {
-                return secondvalue;
-            }
-            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
-            {
-                return thirdvalue;
-            }
-
-
-            throw new Exception("firstNumber,SecondNumber,ThirdNumber are same ");
+//            }
+//            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+//            {
+//                return secondvalue;
+//            }
+//            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+//            {
+//                return thirdvalue;
+//            }
 
 
+//            throw new Exception("firstNumber,SecondNumber,ThirdNumber are same ");
 
 
 
-        }
-        public static double MaximumFloatNumber(double firstvalue, double secondvalue, double thirdvalue)
-        {
-            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0|| firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
-            {
-                return firstvalue;
-
-            }
-            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0|| secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
-            {
-                return secondvalue;
-            }
-            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0|| thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
-            {
-                return thirdvalue;
-            }
-            throw new Exception("firstNumber,SecondNumber,ThirdNumber are same ");
 
 
+//        }
+//        public static double MaximumFloatNumber(double firstvalue, double secondvalue, double thirdvalue)
+//        {
+//            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+//            {
+//                return firstvalue;
 
-        }
-        public static String MaximumStringNumber(String firstvalue, string secondvalue, string thirdvalue)
-        {
-            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 || firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
-            {
-                return firstvalue;
-
-            }
-            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
-            {
-                return secondvalue;
-            }
-            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
-            {
-                return thirdvalue;
-            }
-            throw new Exception("firstNumber,SecondNumber,ThirdNumber are same ");
+//            }
+//            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 || secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+//            {
+//                return secondvalue;
+//            }
+//            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 || thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+//            {
+//                return thirdvalue;
+//            }
+//            throw new Exception("firstNumber,SecondNumber,ThirdNumber are same ");
 
 
 
-        }
-    }
-}
+//        }
+//        public static String MaximumStringNumber(String firststring, string secondstring, string thirdstring)
+//        {
+//            if (firststring.CompareTo(secondstring) > 0 && firststring.CompareTo(thirdstring) > 0 || firststring.CompareTo(secondstring) >= 0 && firststring.CompareTo(thirdstring) > 0 || firststring.CompareTo(secondstring) > 0 && firststring.CompareTo(thirdstring) >= 0)
+//            {
+//                return firststring;
+
+//            }
+//            if (secondstring.CompareTo(firststring) > 0 && secondstring.CompareTo(thirdstring) > 0 || secondstring.CompareTo(firststring) >= 0 && secondstring.CompareTo(thirdstring) > 0 || secondstring.CompareTo(firststring) > 0 && secondstring.CompareTo(thirdstring) >= 0)
+//            {
+//                return secondstring;
+//            }
+//            if (thirdstring.CompareTo(firststring) > 0 && thirdstring.CompareTo(secondstring) > 0 || thirdstring.CompareTo(firststring) >= 0 && thirdstring.CompareTo(secondstring) > 0 || thirdstring.CompareTo(firststring) > 0 && thirdstring.CompareTo(secondstring) >= 0)
+//            {
+//                return thirdstring;
+//            }
+//            throw new Exception("firstNumber,SecondNumber,ThirdNumber are same ");
+            
+
+
+//        }
+//    }
+//}
--- Day13_Generic/Program.cs
+++ Day13_Generic/Program.cs
@@ -9,49 +9,20 @@ namespace Day13_Generic
         {
             Console.WriteLine("-----------------Welcoe to  find Maximum Number By using Generic------------------");
              Console.WriteLine("Find Max Number");
-            
-            int output = MaximumNumberCheck.MaximumIntegerNumber(121, 28, 823);
-            Console.WriteLine(output);
-           //first Test 1.1 the maximum number position is at 1st place
-            int output1 = MaximumNumberCheck.MaximumIntegerNumber(823,28, 121);
-            Console.WriteLine(output);
-           //first Test 1.2 the maximum number position is at 1st place
-            int output2 = MaximumNumberCheck.MaximumIntegerNumber(28, 823, 121);
-            Console.WriteLine(output);
-            //first Test 1.3 the maximum number position is at 1st place
-            int output3 = MaximumNumberCheck.MaximumIntegerNumber(121, 28, 823);
-            Console.WriteLine(output);
-            
-            
-            // for float values checking maximum number
-            double doubleoutput4 = MaximumNumberCheck.MaximumFloatNumber(1.1, 1.2, 1.3);
-            Console.WriteLine(doubleoutput4);
-            // Test2.1
-            double doubleoutput5 = MaximumNumberCheck.MaximumFloatNumber(1.3, 1.2, 1.1);
-            Console.WriteLine(doubleoutput4);
-            // Test2.2
-            double doubleoutput6 = MaximumNumberCheck.MaximumFloatNumber(1.1, 1.3, 1.2);
-            Console.WriteLine(doubleoutput4);
-            //Test2.3
-            double doubleoutput7 = MaximumNumberCheck.MaximumFloatNumber(1.1, 1.2, 1.3);
-            Console.WriteLine(doubleoutput4);
+
+            int[] intArray = { 112, 344, 555 , 457 , 854, 324 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
 
 
+            double[] doubleArray = { 11.2, 3.44, 5.55,45.88,5.2,7.8 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
 
 
-            string stringooutput1 = MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
-            Console.WriteLine(stringooutput1);
-            //Test Case 3.1
-            string stringooutput2 = MaximumNumberCheck.MaximumStringNumber("Peach", "Apple", "Banana");
-            Console.WriteLine(stringooutput1);
-            //Test Case 3.2
-            string stringooutput3 = MaximumNumberCheck.MaximumStringNumber("Apple", "Banana", "Peach");
-            Console.WriteLine(stringooutput1);
-            //Testc Case 3..3
-            string stringooutput4 = MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
-            Console.WriteLine(stringooutput1);
-
-
+            string[] stringArray = { "222","777", "444", "120" , "999" };
+            GenericMaximum<string> genericstring = new GenericMaximum<string>(stringArray);
+            genericstring.PrintMaxValue();
 
         }
     }
